03d8c84eee660bb3afa82cce6330a1d53535a1c2
John Warde <john.warde@gmail.com>
2013-05-08 23:30:32 +0100

    Added assignment data and doc plus initial versions of document and R file

---
+++ D10126532_ProgrammingForBigDataAssignment3_Section2.R
@@ -0,0 +1,34 @@
+#########################################
+#
+# Student ID:   D10126532
+# Student Name: John Warde
+# Course Code:  DT230B
+# 
+# Programming for Big Data Assignment 3
+# 
+# Section 2 Stock Performance
+# 
+#########################################
+
+# TODO: 
+
+# Notes from PDF:
+# * Your solution should contain a textual description (ca. 1 page pdf) of your 
+#   approach to the problem, including any assumptions you make.
+# * Your code must be runnable, in other words it should be possible to source your
+#   script in a brand new R session with the data set in the working directory. If you
+#   use any R libraries (you may or may not), add a comment stating which functions
+#   you are using from that library.
+# * You will get more marks if your code is readable, well-indented, and above all
+#   well-commented.
+# * You will get more marks for appropriate use of the built-in functions of R and its
+#   libraries.
+# Marks
+# * Correctly solve the problem for the 90 day data: up to 20 points
+# * Be able to limit the analysis to a certain number of days: 5 points
+# * If your solution is parallelised: extra 20 points
+# * If your solution uses out-of-memory data: extra 20 points
+
+
+
+
